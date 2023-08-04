d33bfa665617a78562a18058494aef32ede5f38d
Ксения <korzhuk.xeniya@yandex.ru>
2023-08-04 11:20:23 +0300

    task done

--- src/main/java/com/skypro/collections_sheets/CollectionsSheetsApplication.java
+++ src/main/java/com/skypro/collections_sheets/CollectionsSheetsApplication.java
@@ -2,9 +2,13 @@ package com.skypro.collections_sheets;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 public class CollectionsSheetsApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(CollectionsSheetsApplication.class, args);
---
+++ src/main/java/com/skypro/collections_sheets/exceptions/EmployeeStoragelsFullException.java
@@ -0,0 +1,7 @@
+package com.skypro.collections_sheets.exceptions;
+
+public class EmployeeStoragelsFullException extends RuntimeException{
+    public EmployeeStoragelsFullException(String message) {
+        super(message);
+    }
+}
